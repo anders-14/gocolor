21c68e22200acd1be6b17508a02af87f6e87d1a6
anders-14 <anders.fimreite@gmail.com>
2021-03-06 20:05:47 +0100

    working

--- color.go
+++ color.go
@@ -1,50 +1,31 @@
 package gocolor
 
-import "fmt"
-
-// ANSIEscape is an ANSI color escape code
-type ANSIEscape string
-
-var (
-	Reset        ANSIEscape = "\033[0m"
-	Bold         ANSIEscape = "\033[1m"
-	Underline    ANSIEscape = "\033[4m"
-	Blinking     ANSIEscape = "\033[5m"
-	BlackFg      ANSIEscape = "\033[30m"
-	RedFg        ANSIEscape = "\033[31m"
-	GreenFg      ANSIEscape = "\033[32m"
-	BrownFg      ANSIEscape = "\033[33m"
-	BlueFg       ANSIEscape = "\033[34m"
-	PurpleFg     ANSIEscape = "\033[35m"
-	CyanFg       ANSIEscape = "\033[36m"
-	GrayFg       ANSIEscape = "\033[37m"
-	BlackBg      ANSIEscape = "\033[40m"
-	RedBg        ANSIEscape = "\033[41m"
-	GreenBg      ANSIEscape = "\033[42m"
-	BrownBg      ANSIEscape = "\033[43m"
-	BlueBg       ANSIEscape = "\033[44m"
-	PurpleBg     ANSIEscape = "\033[45m"
-	CyanBg       ANSIEscape = "\033[46m"
-	GrayBg       ANSIEscape = "\033[47m"
+import (
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-// Print just like `fmt.Print()`, but with color
-func Print(color ANSIEscape, a ...interface{}) {
-	fmt.Print(string(color))
-	fmt.Print(a...)
-	fmt.Print(string(Reset))
+type rgb struct {
+	r int64
+	g int64
+	b int64
 }
 
-// Printf just like `fmt.Printf()`, but with color
-func Printf(color ANSIEscape, format string, a ...interface{}) {
-	fmt.Print(string(color))
-	fmt.Printf(format, a...)
-	fmt.Print(string(Reset))
+func hexToRGB(hex string) rgb {
+	if strings.HasPrefix(hex, "#") && len(hex) == 7 {
+		hex = hex[1:]
+	}
+	r, _ := strconv.ParseInt(hex[:2], 16, 64)
+	g, _ := strconv.ParseInt(hex[2:4], 16, 64)
+	b, _ := strconv.ParseInt(hex[4:6], 16, 64)
+	return rgb{r, g, b}
 }
 
-// Println just like `fmt.Println()`, but with color
-func Println(color ANSIEscape, a ...interface{}) {
-	fmt.Print(string(color))
-	fmt.Println(a...)
-	fmt.Print(string(Reset))
+func (color rgb) toANSIfg() string {
+	return fmt.Sprintf("\033[38;2;%+v;%+v;%+vm", color.r, color.g, color.b)
+}
+
+func (color rgb) toANSIbg() string {
+	return fmt.Sprintf("\033[48;2;%+v;%+v;%+vm", color.r, color.g, color.b)
 }
--- color_test.go
+++ color_test.go
@@ -1,23 +1,21 @@
-package gocolor_test
+package gocolor
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/anders-14/gocolor"
 )
 
-func TestPrint(t *testing.T) {
-	gocolor.Print(gocolor.GreenFg, "Hi this is a test", "\n")
-	gocolor.Print(gocolor.BlueBg, "Hi this is a test", "\n")
+func TestHexToRGB(t *testing.T) {
+	fmt.Printf("%+v\n", hexToRGB("#33ee33"))
+	fmt.Printf("%+v\n", hexToRGB("#ee3333"))
+	fmt.Printf("%+v\n", hexToRGB("#3333ee"))
+	fmt.Printf("%+v\n", hexToRGB("#FFFFFF"))
+	fmt.Printf("%+v\n", hexToRGB("#000000"))
 }
 
-func TestPrintf(t *testing.T) {
-	name := "Anders"
-	gocolor.Printf(gocolor.GreenFg, "%s is my name\n", name)
-	gocolor.Printf(gocolor.BlueBg, "%s is my name\n", name)
-}
-
-func TestPrintln(t *testing.T) {
-	gocolor.Println(gocolor.GreenFg, "Hi this is a test")
-	gocolor.Println(gocolor.Underline, "Hi this is a test")
+func TestRGBToANSI(t *testing.T) {
+	a := (rgb{r: 255, g: 0, b: 0}).toANSIfg()
+	Println(a, "hei")
+	b := (rgb{r: 255, g: 0, b: 0}).toANSIbg()
+	Println(b, "hei")
 }
